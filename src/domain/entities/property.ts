23cc9642fedfa9e5ffb6bc4106f86fd5f44e2b94
daniel2dfla <daniel_2dfla@hotmail.com>
2025-03-07 13:46:01 -0300

    Feature: Finalizing property entity

---
+++ src/domain/entities/property.ts
@@ -0,0 +1,68 @@
+import { DateRange } from '../value_objects/date_range';
+
+export class Property {
+    constructor(
+        private id: string,
+        private name: string,
+        private description: string,
+        private maxGuests: number,
+        private basePricePerNight: number,
+    ) {
+        this.validateName(name);
+        this.validateNumGuests(maxGuests)
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.maxGuests = maxGuests;
+        this.basePricePerNight = basePricePerNight;
+    }
+
+    getId(): string { 
+        return this.id; 
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getDescription(): string {
+        return this.description;
+    }
+
+    getMaxGuests(): number {
+        return this.maxGuests;
+    }
+
+    getBasePricePerNight(): number {
+        return this.basePricePerNight;
+    }
+
+    validateName(name: string): void {
+        if (name == "") {
+            throw new Error("O nome é obrigatório.");
+        }
+    }
+
+    validateNumGuests(maxGuests: number): void  {
+        if(maxGuests <= 0) {
+            throw new Error("O número máximo de hospedes deve ser um número positivo.");
+        }
+    }
+
+    validateGuestCount(guestCount: number):void {
+        if(guestCount > this.maxGuests) {
+            throw new Error(`Número máximo de hospedes excedido. Máximo permitido: ${this.maxGuests}.`);
+        }
+    }
+
+    calculateTotalPrice(dateRange: DateRange): number {
+        const totalNights = dateRange.getTotalNights();
+        let totalPrice = totalNights * this.basePricePerNight;
+
+        if (totalNights >= 7) {
+            totalPrice *= 0.9;
+        }
+
+        return totalPrice;
+    }
+}
